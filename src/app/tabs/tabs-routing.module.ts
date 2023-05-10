60221013a66a5d5bde6a4c725ab9bf2cb8b1a0c0
Victor Zuluaga <vj_626@hotmail.com>
2023-05-10 11:46:41 +0100

    ahora compila xd

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
-import { RegisterUserComponent } from '../register-user/register-user.component';
 
 const routes: Routes = [
   {
@@ -15,9 +14,6 @@ const routes: Routes = [
       {
         path: 'tab3',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
-      },
-      {
-        path: '', component: RegisterUserComponent
       }
 
     ]
